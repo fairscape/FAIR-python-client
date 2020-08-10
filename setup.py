e1b42f712a7522b764debf18a3e7913d0d3fe173
jniestroy <jniestroy@gmail.com>
2020-08-10 14:31:31 -0400

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='FAIRSCAPE-Client',
+      version='0.1',
+      description='Python Client for FAIRSCAPE',
+      author='Justin Niestroy',
+      author_email='jcn4rh@virginia.edu',
+      packages=['FAIR'],
+      install_requires=['requests'],
+     )
